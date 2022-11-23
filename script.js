20f5d12e63b93abe2f5c7ca86680592869ae6cd3
jdmpictures <74938827+jdmpictures@users.noreply.github.com>
2022-11-23 22:17:04 +0000

    playRound function added

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function playRound (playerSelection, computerSelection){
 
-}
-//Randomises computer choice
+const weapons = ["rock", "paper", "scissors"];
+
 function getComputerChoice(){
     return weapons[Math.floor(Math.random()*weapons.length)]
 }
@@ -15,7 +14,24 @@ function getPlayerChoice(){
         alert("You must enter a valid choice")
         return getPlayerChoice();
     }
-
 }
 
-const weapons = ["rock", "paper", "scissors"]
+function playRound (){
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    console.log(playerSelection);
+    console.log(computerSelection)
+        if(playerSelection == computerSelection){
+            return console.log('It\'s a draw!')
+        }
+        else if(playerSelection == 'rock' && computerSelection == 'paper') {
+            return console.log('Computer wins!')
+        } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+            return console.log('Computer wins!')
+        } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+            return console.log('Computer wins!')
+        } else {
+            return console.log('Player wins!')
+        } 
+    
+}
